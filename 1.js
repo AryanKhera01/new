737086a57722918d3be0d02fb9e887791241ce87
akanshakhanka <akanshakhanka923@gmail.com>
2023-05-19 17:59:58 +0530

    semi calculator

---
+++ 1.js
@@ -0,0 +1,21 @@
+
+function add(){
+let a=parseFloat( prompt("enter the number"));
+let b=parseFloat( prompt("enter the number"));
+document.write(a+b,"<br>");
+}
+function sub(){
+let a=prompt("enter the number");
+let b=prompt("enter the number");
+document.write(a-b,"<br>");
+}
+function mul(){
+let a=prompt("enter the number");
+let b=prompt("enter the number");
+document.write(a*b,"<br>");
+}
+function div(){
+let a=prompt("enter the number");
+let b=prompt("enter the number");
+document.write(a/b,"<br>");
+}
